38a66ce47f1574188cf8b1ed1640bb364a8ec064
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 11:14:06 +0100

    All done

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,6 +1,21 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
+// let numbers = [2,1,3]
 
-for (let numbers[0])
+for (let idx = 0; idx < numbers.length;idx++){
+    
+    for (let ord=idx+1; ord < numbers.length;ord++){
+    
+        if (numbers[idx] > numbers[ord]){
+            let temporal = null;
+            temporal = numbers[idx];
+            numbers[idx] = numbers[ord];
+            numbers[ord] = temporal;
+            console.log(numbers);
+        }       
+    }
+}
+
+
 
 // Put your code here
 //Compara el primer elemento del array con cada uno de los siguientes.
