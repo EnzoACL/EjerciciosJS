528b23b0f8a881af857109460e38a8c8c06d5102
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-25 12:23:02 +0100

    Resolved exercise 6

--- src/functions/exercise6/index.js
+++ src/functions/exercise6/index.js
@@ -2,6 +2,21 @@ const numbers = [11,9,13,12]
 
 // Paste your functions here
 
-// Put your code here
-
-console.log(numbers);
+function swap (posicion1,posicion2,numbers){
+    let temporal = null;
+    temporal = numbers[posicion1];
+    numbers[posicion1] = numbers[posicion2];
+    numbers[posicion2] = temporal;
+}
+function minorInRange (posicion1,posicion2,numbers) {
+    for (idx of numbers)
+        for (idx=posicion1; idx <= posicion2; idx++){
+            if (numbers[idx] > numbers[idx+1] ){
+                swap(idx,idx+1,numbers)
+            }
+        }
+    
+}
+//Se aplica aqui el rango del array que queremos ordenar
+(minorInRange(0,3,numbers)); 
+console.log(numbers); 
